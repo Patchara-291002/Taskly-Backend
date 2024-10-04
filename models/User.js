1f2f281a5ac14ad2e0df9d8a8280df3fcb794c1f
Patchara Kaewnissai <patchara331003@gmail.com>
2024-10-04 23:00:50 +0700

    test

---
+++ models/User.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  googleId: {
+    type: String,
+    required: true,
+  },
+  displayName: {
+    type: String,
+  },
+  email: {
+    type: String,
+  }
+});
+
+module.exports = mongoose.model('User', UserSchema);
