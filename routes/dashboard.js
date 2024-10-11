f7e91ab2b9eb1161e160af3a85a4367db8091bdf
Patchara Kaewnissai <patchara331003@gmail.com>
2024-10-11 18:54:22 +0700

    try to create project by user

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
+const Project = require('../models/Project');
 const passport = require('passport');
 
 require('dotenv').config();
@@ -18,5 +19,17 @@ router.get('/', async (req, res) => {
         return res.redirect('/auth/google');
     }
 });
+
+router.get('/projects', async (req, res) => {
+    try {
+        const userId = req.query.userId; 
+        const projects = await Project.find({ users: userId }).populate('users');
+        res.status(200).json(projects);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to fetch projects' });
+    }
+});
+
+
 
 module.exports = router;
